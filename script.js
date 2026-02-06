11545e76321b25a2215f1a53cfda9657679fc9a7
manyasuvarna14 <manyasuvarna02@gmail.com>
2026-02-06 20:59:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let isScientific = false;
+
 function append(value) {
     document.getElementById("result").value += value;
 }
@@ -9,4 +11,32 @@ function clearResult() {
 function calculate() {
     let expression = document.getElementById("result").value;
     document.getElementById("result").value = eval(expression);
+}
+
+function toggleMode() {
+    isScientific = !isScientific;
+
+    document.getElementById("simpleButtons").style.display =
+        isScientific ? "none" : "block";
+
+    document.getElementById("scientificButtons").style.display =
+        isScientific ? "block" : "none";
+
+    document.querySelector(".toggle").innerText =
+        isScientific ? "Switch to Simple" : "Switch to Scientific";
+}
+
+function scientific(func) {
+    let value = document.getElementById("result").value;
+    document.getElementById("result").value = eval(func + "(" + value + ")");
+}
+
+function square() {
+    let value = document.getElementById("result").value;
+    document.getElementById("result").value = value * value;
+}
+
+function power() {
+    let value = document.getElementById("result").value;
+    document.getElementById("result").value = Math.pow(value, 2);
 }
