4f604c3abfa98af4b32632c370ce68b919dff8ea
manyasuvarna14 <manyasuvarna02@gmail.com>
2026-02-08 13:54:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,39 @@
 let isScientific = false;
 
-document.addEventListener("keydown", function(event) {
-    const allowed = "0123456789+-*/.";
-    if (allowed.includes(event.key)) {
-        append(event.key);
-    } else if (event.key === "Enter") {
-        calculate();
-    } else if (event.key === "Backspace") {
-        let box = document.getElementById("result");
-        box.value = box.value.slice(0, -1);
-    }
-});
-
 function append(value) {
     document.getElementById("result").value += value;
 }
 
-function clearAll() {
+function clearResult() {
     document.getElementById("result").value = "";
-    document.getElementById("history").innerText = "";
 }
 
 function calculate() {
     let expression = document.getElementById("result").value;
-    let answer = eval(expression);
-
-    document.getElementById("history").innerText = expression + " =";
-    document.getElementById("result").value = answer;
+    document.getElementById("result").value = eval(expression);
 }
 
 function toggleMode() {
     isScientific = !isScientific;
 
     document.getElementById("simpleButtons").style.display =
-        isScientific ? "none" : "block";
+        isScientific ? "none" : "grid";
 
     document.getElementById("scientificButtons").style.display =
-        isScientific ? "block" : "none";
+        isScientific ? "grid" : "none";
 }
 
 function scientific(func) {
     let value = document.getElementById("result").value;
-    let result = eval(func + "(" + value + ")");
-    document.getElementById("history").innerText = func + "(" + value + ") =";
-    document.getElementById("result").value = result;
+    document.getElementById("result").value = eval(func + "(" + value + ")");
 }
 
 function square() {
     let value = document.getElementById("result").value;
-    document.getElementById("history").innerText = value + "² =";
     document.getElementById("result").value = value * value;
 }
 
 function power() {
     let value = document.getElementById("result").value;
-    document.getElementById("history").innerText = value + "^2 =";
     document.getElementById("result").value = Math.pow(value, 2);
 }
